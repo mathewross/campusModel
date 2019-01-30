5cf48b62648fcd3e56a2f8194b32d628e1a376b2
mathewross <matross92@gmail.com>
2019-01-30 19:32:08 +0000

    First Project Commit

---
+++ Unity Files/attempt2/Assets/Scripts/DropDown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDown : MonoBehaviour
+{
+    List<string> names = new List<string>() { "Please Select A Building", "Harold Cohen", "Electrical Eng", "Ashton" };
+
+
+    public Dropdown dropdown;
+    public Text selectedBuilding;
+    public Button beginButton;
+
+
+    public void Dropdown_INdexChanged(int index)
+    {
+        selectedBuilding.text = names[index];
+        if(index == 0)
+        {
+            beginButton.enabled = false;
+        }
+        else
+        {
+            beginButton.enabled = true;
+        }
+    }
+
+    void Start()
+    {
+        beginButton.enabled = false;
+        PopulateList();
+    }
+
+    void PopulateList()
+    {
+        dropdown.AddOptions(names);
+    }
+
+}
